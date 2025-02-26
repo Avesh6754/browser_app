3731839f63f01302bed24424facdf710fc2f9adb
R23 <aweshprajapati150@gmail.com>
2025-02-26 09:48:47 +0530

    2-26-2025

--- lib/controller/homeController.dart
+++ lib/controller/homeController.dart
@@ -1,28 +1,95 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:get/get.dart';
-
-var getController = Get.put(WebGetController());
+import 'package:shared_preferences/shared_preferences.dart';
 
 class WebGetController extends GetxController {
-  var txtSearch = TextEditingController();
-  InAppWebViewController? webViewController;
   RxString selectedSearchEngine = "https://www.google.com/search?q=".obs;
+  RxString selectedSearchEngineName = "Google".obs;
 
-  List imageList = [
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png',
-    'https://w7.pngwing.com/pngs/423/261/png-transparent-bing-hd-logo-thumbnail.png',
-    'https://w7.pngwing.com/pngs/674/470/png-transparent-yandex-logo-y-search-engines-thumbnail.png',
-  ];
-
-  RxList linkList = [
+  List<String> linkList = [
     "https://www.google.com/search?q=",
     "https://www.bing.com/search?q=",
-    "https://yandex.com/search/?text=",
-  ].obs;
+    "https://search.yahoo.com/search?p=",
+  ];
+
+  List<String> searchEngineNames = ["Google", "Bing", "Yahoo"];
+
+  List<String> imageList = [
+    "https://upload.wikimedia.org/wikipedia/commons/4/4a/Google_2015_logo.svg",
+    "https://upload.wikimedia.org/wikipedia/commons/9/9c/Bing_logo_%282016%29.svg",
+    "https://upload.wikimedia.org/wikipedia/commons/2/24/Yahoo%21_logo.svg",
+  ];
+
+  TextEditingController txtSearch = TextEditingController();
+  InAppWebViewController? webViewController;
+
+  RxList<String> history = <String>[].obs;
+  RxList<String> bookmarks = <String>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    loadHistory();
+    loadBookmarks();
+  }
+
+  // Load browsing history
+  Future<void> loadHistory() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    history.value = prefs.getStringList("history") ?? [];
+  }
+
+  // Save browsing history
+  Future<void> saveHistory(String url) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (!history.contains(url)) {
+      history.add(url);
+      await prefs.setStringList("history", history);
+    }
+  }
+
+  // Clear history
+  Future<void> clearHistory() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    history.clear();
+    await prefs.remove("history");
+  }
+
+  // Load bookmarks
+  Future<void> loadBookmarks() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bookmarks.value = prefs.getStringList("bookmarks") ?? [];
+  }
+
+  // Add bookmark
+  Future<void> addBookmark(String url) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (!bookmarks.contains(url)) {
+      bookmarks.add(url);
+      await prefs.setStringList("bookmarks", bookmarks);
+    }
+  }
+
+  // Remove bookmark
+  Future<void> removeBookmark(String url) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bookmarks.remove(url);
+    await prefs.setStringList("bookmarks", bookmarks);
+  }
 
   void getWebsiteLink(int index) {
     selectedSearchEngine.value = linkList[index];
-    update(); // Ensures UI is updated
+    selectedSearchEngineName.value = searchEngineNames[index];
+    update();
+    loadWebPage();
+  }
+
+  void loadWebPage() {
+    if (webViewController != null) {
+      String url = "${selectedSearchEngine.value}${txtSearch.text}";
+      webViewController!.loadUrl(urlRequest: URLRequest(url: WebUri(url)));
+      saveHistory(url);
+    }
   }
 }
--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,12 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_adv_ch4/views/history_page.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:get/get.dart';
-
 import '../controller/homeController.dart';
-
-
+final WebGetController getController = Get.put(WebGetController());
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,126 +13,141 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          actions: [
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 8, right: 8),
-                child: Card(
-                  color: Colors.grey.shade200,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextField(
-                      onChanged: (value) {
-                        getController.webViewController!.loadUrl(
-                          urlRequest: URLRequest(
-                            url: WebUri(
-                            "${getController.selectedSearchEngine}${getController.txtSearch.text}",
-                          ),
-                        ));
-                      },
-                      controller: getController.txtSearch,
-                      decoration: InputDecoration(
-                        prefixIcon: const Icon(Icons.search),
-                        hintText: "Search or enter URL",
-                        border: InputBorder.none,
-                        suffixIcon: IconButton(
-                          onPressed: () {
-                            getController.webViewController!.reload();
-                          },
-                          icon: Icon(Icons.history_outlined),
-                        ),
+      appBar: AppBar(
+        title: Obx(() => Text(getController.selectedSearchEngineName.value)),
+        actions: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 8, right: 8),
+              child: Card(
+                color: Colors.grey.shade200,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+
+                    controller: getController.txtSearch,
+                    decoration: InputDecoration(
+
+                      prefixIcon:  IconButton(onPressed: () {
+                        getController.loadWebPage();
+                      }, icon: Icon(Icons.search)),
+                      hintText: "Search or enter URL",
+                      border: InputBorder.none,
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          getController.webViewController!.reload();
+                        },
+                        icon: const Icon(Icons.refresh),
                       ),
                     ),
                   ),
                 ),
               ),
             ),
-          ],
-        ),
-        body: GetBuilder<WebGetController>(builder: (controller) {
-          return
-          InAppWebView(
-
-            initialUrlRequest: URLRequest(
-              url: WebUri(
-                "${controller.selectedSearchEngine}${controller.txtSearch.text}",
+          ),
+          PopupMenuButton<int>(
+            onSelected: (index) {
+              getController.getWebsiteLink(index);
+            },
+            itemBuilder: (context) => List.generate(
+              getController.linkList.length,
+                  (index) => PopupMenuItem(
+                value: index,
+                child: Row(
+                  children: [
+                    Image.network(getController.imageList[index], width: 24, height: 24),
+                    const SizedBox(width: 10),
+                    Text(getController.searchEngineNames[index]),
+                  ],
+                ),
               ),
             ),
-            onWebViewCreated: (controller) {
-              getController.webViewController = controller;
-            },
-          );
-        },),
-        bottomNavigationBar: BottomNavigationBar(items: [
-          BottomNavigationBarItem(
-              label: 'Back',
-              icon: IconButton(
-                  onPressed: () {
-                    getController.webViewController!.goBack();
-                  },
-                  icon: const Icon(Icons.arrow_back))),
-          BottomNavigationBarItem(
-              label: 'Forward',
-              icon: IconButton(
-                  onPressed: () {
-                    getController.webViewController!.goForward();
-                  },
-                  icon: const Icon(Icons.arrow_forward_outlined))),
-          BottomNavigationBarItem(
-              label: 'Menu',
-              icon: IconButton(
-                  onPressed: () {
-                    showModalBottomSheet(
+            icon: const Icon(Icons.more_vert),
+          ),
 
-                      context: context,
-                      builder: (context) {
-                        return Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                           ...List.generate(getController.imageList.length, (index) {
-                              return GestureDetector(
-                                onTap: () {
-                                  getController.getWebsiteLink(index);
-                                  getController.webViewController!.loadUrl(
-                                      urlRequest: URLRequest(
-                                        url: WebUri(
-                                          "${getController.selectedSearchEngine}",
-                                        ),
-                                      ));
-                                 Navigator.pop(context);
-                                },
-                                child: webMethod(image: getController.imageList[index]),
-                              );
-                            }),
-                          ],
-                        );
-                      },
-                    );
-                  },
-                  icon: const Icon(Icons.menu)))
-        ]));
-  }
-
-  Container webMethod({required String image,var index}) {
-    return Container(
-      height: 50,
-      width: 50,
-      margin: const EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        color: Colors.grey.shade100,
-        shape: BoxShape.circle,
-        image: DecorationImage(
-          image: NetworkImage(image),
+          IconButton(onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => HistoryPage(),));
+          }, icon: Icon(Icons.history)),
+          IconButton(
+            icon: const Icon(Icons.bookmark),
+            onPressed: () => showBookmarks(context),
+          ),
+        ],
+      ),
+      body: Obx(
+            () => InAppWebView(
+          initialUrlRequest: URLRequest(
+            url: WebUri("${getController.selectedSearchEngine.value}${getController.txtSearch.text}"),
+          ),
+          onWebViewCreated: (controller) {
+            getController.webViewController = controller;
+          },
         ),
       ),
-      child: const SizedBox(
-        height: 60,
-        width: 60,
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            label: 'Back',
+            icon: IconButton(
+              onPressed: () => getController.webViewController!.goBack(),
+              icon: const Icon(Icons.arrow_back),
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: 'Forward',
+            icon: IconButton(
+              onPressed: () => getController.webViewController!.goForward(),
+              icon: const Icon(Icons.arrow_forward_outlined),
+            ),
+          ),
+
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: IconButton(
+              onPressed: () => getController.loadWebPage(),
+              icon: const Icon(Icons.home),
+            ),
+          ),
+        ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+         await getController.addBookmark(getController.txtSearch.text);
+        },
+        child: const Icon(Icons.bookmark_add),
+      ),
+
     );
   }
+}
+
+void showBookmarks(BuildContext context) {
+  showModalBottomSheet(
+    context: context,
+    builder: (context) {
+      return Obx(
+            () => ListView.builder(
+          itemCount: getController.bookmarks.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(getController.bookmarks[index]),
+              trailing: IconButton(
+                icon: const Icon(Icons.delete),
+                onPressed: () => getController.removeBookmark(getController.bookmarks[index]),
+              ),
+              onTap: () {
+                getController.txtSearch.text = getController.bookmarks[index];
+                getController.loadWebPage();
+                Navigator.pop(context);
+              },
+            );
+          },
+        ),
+      );
+    },
+  );
 }
